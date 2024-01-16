0ce887834483c37ad1b9bc30a35bac59e7cccf88
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2024-01-16 21:36:04 +0200

    error handling, delete and update options added to note

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -22,6 +22,17 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+//middleware for error handlng 
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+
+    if(error.name === 'CastError'){
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+
 app.use(requestLogger)
 
 {/*let notes = [
@@ -68,28 +79,60 @@ app.get('/app/notes/:id', (req, res) => {
 */}
 
 // fetching note from mongo using findById
-app.get('api/notes/:id', (req, res) => {
-    Note.findById(req.params.id).then(note => {
-        res.json(note)
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+        .then(note => {
+            if (note){
+                res.json(note)
+            } else {
+                res.status(404).end()
+            }
     })
+    .catch(error => next(error) )
 })
 
-app.delete('/app/notes/:id', (req, res) => {
+// update data on MongoDB
+app.put('/api/notes/:id', (req, res, next) => {
+    const body = req.body
+
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(req.params.id, note, {new: true})
+        .then(updatedNote => {
+            res.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+{/*app.delete('/app/notes/:id', (req, res) => {
     const id = Number(req.params.id)
 
     notes = notes.filter(note => note.id !== id)
 
     res.status(204).end()
 })
+*/}
 
-const generateId = () => {
+// delete operation on MongoDB data
+app.delete('/app/notes/:id', (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+{/*const generateId = () => {
     const maxId = notes.length > 0
                     ? Math.max(...notes.map(n => n.id))
                     : 0
     return maxId + 1
 }
 
-{/*app.post('/app/notes', (req, res) => {
+app.post('/app/notes', (req, res) => {
     const body = req.body
 
     if(!body.content){
@@ -129,6 +172,9 @@ app.post('/api/notes', (req, res) => {
 })
 
 app.use(unknownEndpoint)
+
+//call error handler middleware at last of the code 
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
